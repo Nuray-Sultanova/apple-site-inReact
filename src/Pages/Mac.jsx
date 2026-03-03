c178fb42e39653d0f2a4b99d82e59f0f30eca558
Nuray-Sultanova <sultanovanuray448@gmail.com>
2026-03-04 00:10:21 +0400

    mac page created

---
+++ src/Pages/Mac.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import MacAnimasiya from "../Components/MacAnimasiya";
+
+const Mac = () => {
+  return (
+    <div className="max-w-5xl mx-auto ">
+      <div className="max-w-4xl mx-auto py-4 text-center">
+        <p className="font-md  text-xs">
+          The Apple Shopping Event is coming. From November 25 to 28 get an
+          Apple Gift Card when you buy an eligible product.**
+        </p>
+      </div>
+      <MacAnimasiya />
+    </div>
+  );
+};
+
+export default Mac;
