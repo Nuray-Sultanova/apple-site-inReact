f1e3ee0ed3f05c54c6a9951da646eb353597798b
Nuray-Sultanova <sultanovanuray448@gmail.com>
2026-03-04 00:09:09 +0400

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,33 @@ import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
 import Home from "./Pages/Home";
 import Accessories from "./Pages/Accessories";
+import Mac from "./Pages/Mac";
 
 function App() {
   return (
     <div className="antialiased font-sans bg-white">
-      {/* Navbar həmişə başda durur */}
       <div className="w-full z-50">
         <Navbar />
       </div>
-
-      {/* Səhifə dəyişəndə yalnız bura dəyişir */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/accessories" element={<Accessories />} />
-      </Routes>
-
-      {/* Footer həmişə sonda durur */}
+      {/*very important*/}
+      <div className="pt-12">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/accessories" element={<Accessories />} />
+          <Route path="/mac" element={<Mac />} />
+        </Routes>
+      </div>
       <footer className="bg-[#f5f5f7] py-16 mt-10 border-t border-gray-200">
         <div className="max-w-5xl mx-auto px-6">
           <div className="text-[12px] text-[#6e6e73] leading-relaxed space-y-4 border-b border-gray-200 pb-10 mb-10">
-            <p>1. Offer valid on qualifying purchases of eligible Apple products from November 25, 2022, to November 28, 2022.</p>
-            <p>Apple Card is issued by Goldman Sachs Bank USA, Salt Lake City Branch.</p>
+            <p>
+              1. Offer valid on qualifying purchases of eligible Apple products
+              from November 25, 2022, to November 28, 2022.
+            </p>
+            <p>
+              Apple Card is issued by Goldman Sachs Bank USA, Salt Lake City
+              Branch.
+            </p>
           </div>
           <Footer />
         </div>
